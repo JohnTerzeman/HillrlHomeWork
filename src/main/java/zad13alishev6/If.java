18c9a7027a2f7aa5cb3124d1d84413ae4b6cc249
Джон <knopa.durinda@gmail.com>
2023-07-15 23:01:24 +0300

    zad13

---
+++ src/main/java/zad13alishev6/If.java
@@ -0,0 +1,12 @@
+package zad13alishev6;
+
+public class If {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x < 10) {
+            System.out.println("true");
+        } else if (x < 20) {
+            System.out.println("false");
+        }
+    }
+}
