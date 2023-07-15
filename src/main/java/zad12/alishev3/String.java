95954e7bc8241f9a2ea0e466ed96ad01dbd86017
Джон <knopa.durinda@gmail.com>
2023-07-15 22:08:57 +0300

    zad12

---
+++ src/main/java/zad12/alishev3/String.java
@@ -0,0 +1,13 @@
+package zad12.alishev3;
+
+public class String {
+    public static void main(String[] args){
+        int x = 5;
+        String s = "Hello world";
+        String spase = " ";
+        String name = "Bob";
+        System.out.println(s+spase+name);
+        System.out.println("Hello" + "" + "John");
+        System.out.println("My number is" + x);
+    }
+}
