837446e1dd2107e61f874b44fbc769e5b23cc6b1
Джон <knopa.durinda@gmail.com>
2023-08-01 23:40:56 +0300

    zad18

---
+++ src/main/java/zad18alishev31/Main.java
@@ -0,0 +1,10 @@
+package zad18alishev31;
+
+public class Main {
+    public static void main(String[] args){
+        byte x =(byte )128;
+        System.out.println(x);
+
+
+    }
+}
---
+++ src/main/java/zad18alishev33/Main.java
@@ -0,0 +1,10 @@
+package zad18alishev33;
+
+public class Main {
+    public static void main(String[] args){
+        Animal a = new Animal();
+        Dog dog = (Dog) a;
+        dog.bark();
+    }
+}
+
